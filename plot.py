242f9ac8360ca042024b9699d2e0bc17739f790d
Yi Li <liyi@cs.toronto.edu>
2015-10-14 15:16:35 -0400

    change plotting style

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-pd.set_option('display.width', 100)
+pd.set_option('display.width', 120)
 import argparse
 
 def plot(stats, players):
@@ -14,7 +14,7 @@ def plot(stats, players):
     tuples.append((player, p_name))
 
   index = pd.MultiIndex.from_tuples(tuples, names=['id','name'])
-  trans[index].plot(kind='line', use_index=False)
+  trans[index].plot(kind='line', grid=True, use_index=True)
 
   plt.show()
 
@@ -24,9 +24,12 @@ def main():
                       type=int, nargs='*', help='ids of players to display')
   parser.add_argument('-d', '--display', type=int, \
                       choices=[10,25,50], default=10, help='number of rows to display')
+  parser.add_argument('-n', '--number', type=int, \
+                      choices=[1,2,3,4,5,6,7], default=5, help='number of columns to display')
   args = parser.parse_args()
 
   show = int(args.display)
+  length = int(args.number)
   stats = pd.DataFrame.from_csv('.cache/res.csv')
   
   if len(args.players) > 0:
